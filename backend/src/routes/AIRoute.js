46d33c410983db6f52d6b57577a6792fc1c07da4
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-03-20 22:32:37 +0700

    added validator middleware and send prompt route

---
+++ backend/src/routes/AIRoute.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+const router = Router();
+import AIController from '../controllers/AIController.js';
+import ValidatorMiddleware from '../middleware/ValidatorMiddleware.js';
+
+router.all('/send-prompt',
+	ValidatorMiddleware.validateMethods(['POST']),
+	ValidatorMiddleware.validateAccessToken,
+	ValidatorMiddleware.validateContentType,
+	AIController.sendPrompt
+);
+
+export default router;
