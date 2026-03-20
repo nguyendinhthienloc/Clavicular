e15e0381c9ada75fc20ca9d6463fe8e970d19068
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-03-20 22:27:53 +0700

    implemented basic AI send prompt endpoint

---
+++ backend/src/services/AIService.js
@@ -0,0 +1,46 @@
+import config from '../config/config.js'
+import ServiceResponse from '../helper/ServiceResponse.js';
+import { GoogleGenAI } from '@google/genai';
+const gemini = new GoogleGenAI(config.gemini.APIkey);
+
+class AIService {
+	constructor() {
+		
+	}
+
+	async sendPrompt(prompt, model = 'gemini-flash-latest') {
+		try {
+			await gemini.models.get({ model: model });
+		} catch (err) {
+			const response = new ServiceResponse(
+				false,
+				422,
+				"Cannot load model",
+				err.toString()
+			);
+			return response;
+		}
+		try {
+			const data = await gemini.models.generateContent({
+				model: module,
+				contents: prompt
+			});
+			const text = data.candidates[0].content.parts[0].text;
+			const response = new ServiceResponse(
+				true,
+				200,
+				"Success",
+				text
+			);
+			return response;
+		} catch (err) {
+			const response = new ServiceResponse(
+				false,
+				502,
+				"Something went wrong",
+				err.toString()
+			);
+			return response;
+		}
+	}
+}
